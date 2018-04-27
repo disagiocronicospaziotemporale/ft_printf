2715823801d35105809239239d74b21e780e89d3
Merlotti RICCARDO <mriccard@e2r3p19.42.fr>
2018-04-27 19:56:49 +0200

    bha

--- spaces_manager.c
+++ spaces_manager.c
@@ -6,7 +6,7 @@
 /*   By: mriccard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/24 19:25:15 by mriccard          #+#    #+#             */
-/*   Updated: 2018/04/24 19:43:08 by mriccard         ###   ########.fr       */
+/*   Updated: 2018/04/27 19:56:25 by mriccard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,13 @@
 
 void size_and_precision_manager(unsigned int *flags, char *str)
 {
-	
+	unsigned int l;
+	unsigned int minus;
+
+	l = ft_strlen(str);
+	if (str[0] == '-')
+		minus = 1;
+	else
+		minus = 0;
+	if((l - minus) > flags[6])
 }
